1f96c95c99acd7e309ac1d140f6d6328c0d7d738
superarius <superarius@gmail.com>
2021-05-26 12:49:51 -0700

    MAINNET IMPLEMENTATION DEPLOYMENT

--- scripts/poolInfo.ts
+++ scripts/poolInfo.ts
@@ -62,21 +62,30 @@ const op = async (signer: SignerWithAddress) => {
   const { tick, sqrtPriceX96, observationCardinality } = await pool.slot0();
   const feeGlobal0 = await pool.feeGrowthGlobal0X128();
   const feeGlobal1 = await pool.feeGrowthGlobal1X128();
-  console.log("current tick:", tick.toString());
-  console.log("current sqrtPrice:", sqrtPriceX96.toString());
-  console.log("cardinality:", observationCardinality.toString());
+  console.log("----POOL INFO----");
+  console.log(
+    `current tick: ${tick.toString()} (price: $${(
+      1 /
+      1.0001 ** Number(tick)
+    ).toFixed(2)})`
+  );
+  console.log("twap obs cardinality:", observationCardinality.toString());
 
   const { tickCumulatives } = await pool.observe([1800, 0]);
   const positionLower = await gelatoUniV3Pool.currentLowerTick();
   const positionUpper = await gelatoUniV3Pool.currentUpperTick();
+  const avgTick = tickCumulatives[1]
+    .sub(tickCumulatives[0])
+    .div(ethers.BigNumber.from("1800"));
+  const avgPrice = 1 / 1.0001 ** Number(avgTick.toString());
   console.log(
-    "thirty min avg tick:",
-    tickCumulatives[1]
-      .sub(tickCumulatives[0])
-      .div(ethers.BigNumber.from("1800"))
-      .toString()
+    `thirty min avg tick: ${avgTick.toString()} (price: $${avgPrice.toFixed(
+      2
+    )})`
   );
 
+  console.log("");
+  console.log("----POSITION INFO----");
   // eslint-disable-next-line
   const { _liquidity, tokensOwed0, tokensOwed1 } = await pool.positions(
     await gelatoUniV3Pool.getPositionID()
@@ -115,19 +124,52 @@ const op = async (signer: SignerWithAddress) => {
     Number(positionLower),
     Number(positionUpper)
   );
+  const sqrtPriceLower = BigNumber.from(
+    (Math.sqrt(1.0001 ** Number(positionLower)) * 2 ** 36).toFixed(0)
+  ).mul(BigNumber.from("2").pow(BigNumber.from("60")));
+  const sqrtPriceUpper = BigNumber.from(
+    (Math.sqrt(1.0001 ** Number(positionUpper)) * 2 ** 36).toFixed(0)
+  ).mul(BigNumber.from("2").pow(BigNumber.from("60")));
+  const { amount0, amount1 } = await gelatoUniV3Pool.getAmountsForLiquidity(
+    sqrtPriceX96,
+    sqrtPriceLower,
+    sqrtPriceUpper,
+    _liquidity
+  );
 
-  const lower = await gelatoUniV3Pool.currentLowerTick();
-  const upper = await gelatoUniV3Pool.currentUpperTick();
-  console.log("lower tick:", lower.toString());
-  console.log("upper tick:", upper.toString());
+  const weth = await ethers.getContractAt(
+    ["function balanceOf(address) external view returns(uint256)"],
+    addresses.WETH,
+    signer
+  );
+  const dai = await ethers.getContractAt(
+    ["function balanceOf(address) external view returns(uint256)"],
+    addresses.DAI,
+    signer
+  );
+
+  const balanceWeth = await weth.balanceOf(gelatoUniV3Pool.address);
+  const balanceDai = await dai.balanceOf(gelatoUniV3Pool.address);
+  const lowerPrice = 1 / 1.0001 ** Number(positionLower);
+  const upperPrice = 1 / 1.0001 ** Number(positionUpper);
+  console.log(
+    `lower tick: ${positionLower.toString()} (price: $${lowerPrice.toFixed(2)})`
+  );
+  console.log(
+    `upper tick: ${positionUpper.toString()} (price: $${upperPrice.toFixed(2)})`
+  );
+  console.log("DAI invested:", ethers.utils.formatEther(amount0));
+  console.log("WETH invested:", ethers.utils.formatEther(amount1));
   console.log(
     "unclaimed DAI fees:",
-    ethers.utils.formatEther(fee0.add(tokensOwed0).toString())
+    ethers.utils.formatEther(fee0.add(tokensOwed0))
   );
   console.log(
     "unclaimed WETH fees:",
-    ethers.utils.formatEther(fee1.add(tokensOwed1).toString())
+    ethers.utils.formatEther(fee1.add(tokensOwed1))
   );
+  console.log("DAI uninvested:", ethers.utils.formatEther(balanceDai));
+  console.log("WETH uninvested:", ethers.utils.formatEther(balanceWeth));
 };
 
 (async () => {
